50918f8a52ba6d3bbe3bd5ae8f35065e645a206b
ClydeFrog04 <egan.randal@gmail.com>
2020-04-28 19:35:19 -0600

    Get yesterday's photo working

--- src/App.js
+++ src/App.js
@@ -6,11 +6,7 @@ import axios from "axios";
 
 function App() {
     //get today in string form for our api request
-    let now = new Date();//get today so we can use it in our api call. This is needed for getting past images.
-    let dd = String(now.getDate()).padStart(2, '0');
-    let mm = String(now.getMonth() + 1).padStart(2, '0'); //January is 0!
-    let yyyy = now.getFullYear();
-    now = `${yyyy}-${mm}-${dd}`;
+    let now = getDateString(new Date());//get today so we can use it in our api call. This is needed for getting past images.
 
     const [data, setData] = useState({});
     const [date, setDate] = useState(now);
@@ -27,9 +23,17 @@ function App() {
     return (
         <div className="App">
             <ImageContainer imageData={data}/>
-            <ControlContainer imageData={data}/>
+            <ControlContainer imageData={data} getDateString={getDateString} setDate={setDate}/>
         </div>
     );
+}
+
+const getDateString = (dateRequested) =>{
+    let dd = String(dateRequested.getDate()).padStart(2, '0');
+    let mm = String(dateRequested.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = dateRequested.getFullYear();
+    dateRequested = `${yyyy}-${mm}-${dd}`;
+    return dateRequested;
 }
 
 export default App;
--- src/Components/ButtonContainer/ControlContainer.js
+++ src/Components/ButtonContainer/ControlContainer.js
@@ -3,9 +3,17 @@ import "./ControlContainer.css";
 
 const ControlContainer = props => {
 
+    const setDateYesterday = () => {
+        let yesterday = new Date();
+        yesterday.setSeconds(yesterday.getSeconds() - 86400);//86400 is the number of seconds in a day. This way I don't have to worry about changing month day and year if yesterday was in a different month or year
+        yesterday = props.getDateString(yesterday);
+        console.log(yesterday);
+        props.setDate(yesterday);
+    }
+
     return (
         <div className="controlContainer">
-            <button className="yesterdayImage">Yesterday's APOD</button>
+            <button className="yesterdayImage" onClick={setDateYesterday}>Yesterday's APOD</button>
             <button className="olderImage">Placeholder For Dropdown</button>
             <button className="randImg">Random APOD</button>
         </div>
